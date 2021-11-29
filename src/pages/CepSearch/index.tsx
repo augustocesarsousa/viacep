37f395aec5f4dbc98216792fc7cc86bb7f3f64b2
augusto cesar <acs-03@hotmail.com>
2021-11-29 16:31:42 -0300

    Form event

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,19 +1,53 @@
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+
+type FormData = {
+  cep: string;
+  test: string;
+}
 
 const CepSearch = () => {
+
+  const [formData, setFomrData] = useState<FormData>({
+    cep: '',
+    test: ''
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFomrData({...formData, [name]:value});
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="cep-search-container">
       <h1 className="text-primary">Busca CEP</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
+              name="cep"
+              value={formData.cep}
               className="search-input"
               placeholder="CEP (somente números)"
-              onChange={() => {}}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="test"
+              value={formData.test}
+              className="search-input"
+              placeholder="test"
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Buscar
@@ -23,7 +57,6 @@ const CepSearch = () => {
 
         <ResultCard title="Logradouro" description="Lalala" />
         <ResultCard title="Número" description="234" />
-
       </div>
     </div>
   );
